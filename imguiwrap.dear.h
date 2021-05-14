7ec11477f1d20ad9e070e9e94fe36f9d96d65a52
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-05-14 16:51:34 -0700

    Reworked wrappers to generate better code

--- imguiwrap.dear.h
+++ imguiwrap.dear.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <functional>
-
 #include "imgui.h"
 
 namespace dear
@@ -14,58 +12,70 @@ namespace dear
 //
 // On its own, it does nothing but call the supplied function when it is
 // destroyed;
-struct scoped_effect
+template<typename Base, bool ForceDtor=false>
+struct ScopeWrapper
 {
-    using code_t = std::function<void(void)>;
+    using wrapped_type = Base;
+    using self_type = ScopeWrapper<Base>;
+
+    static constexpr bool force_dtor = ForceDtor;
 
 protected:
-    const code_t atEnd_;
     const bool ok_;
-    const bool mustClose_;
 
 public:
     // constructor takes a predicate that may be used to determine if
     // additional calls can be made, and a function/lambda/callable to
     // be invoked from the destructor.
-    scoped_effect(bool ok, code_t atEnd, bool mustClose=false) noexcept : ok_{ok}, atEnd_{atEnd}, mustClose_{mustClose} {}
+    constexpr ScopeWrapper(bool ok) noexcept : ok_{ok} {}
 
     // destructor always invokes the supplied destructor function.
-    ~scoped_effect() noexcept {
-        if (ok_ || mustClose_) atEnd_();
+    constexpr ~ScopeWrapper() noexcept {
+        if constexpr (!force_dtor) {
+            if (!ok_)
+                return;
+        }
+        Base::dtor();
     }
 
     // operator&& will excute 'code' if the predicate supplied during
     // construction was true.
-    bool operator&&(code_t code) const
+    template<typename PassthruFn>
+    constexpr bool operator&&(PassthruFn passthru) const
     {
         if (ok_)
-            code();
+            passthru();
         return ok_;
     }
 
-    operator bool() const noexcept { return ok_; }
+    constexpr operator bool() const noexcept { return ok_; }
 
 protected:
-    scoped_effect(const scoped_effect&) = delete;
-    scoped_effect& operator=(const scoped_effect&) = delete;
+    ScopeWrapper(const ScopeWrapper&) = delete;
+    ScopeWrapper& operator=(const ScopeWrapper&) = delete;
 };
 
-struct Begin : public scoped_effect
+struct Begin : public ScopeWrapper<Begin, true>
 {
     // Invoke Begin and guarantee that 'End' will be called.
     Begin(const char* title, bool* open=nullptr, ImGuiWindowFlags flags=0)
-        : scoped_effect(ImGui::Begin(title, open, flags), ImGui::End, true)
+        : ScopeWrapper(ImGui::Begin(title, open, flags))
         {}
+    static constexpr void(*dtor)() = ImGui::End;
 };
 
-struct TabBar : public scoped_effect
+struct TabBar : public ScopeWrapper<TabBar>
 {
-    TabBar(const char* name, ImGuiTabBarFlags flags=0) : scoped_effect(ImGui::BeginTabBar(name, flags), ImGui::EndTabBar) {}
+    TabBar(const char* name, ImGuiTabBarFlags flags=0)
+        : ScopeWrapper(ImGui::BeginTabBar(name, flags)) {}
+    static constexpr void(*dtor)() = ImGui::EndTabBar;
 };
 
-struct TabItem : public scoped_effect
+struct TabItem : public ScopeWrapper<TabItem>
 {
-    TabItem(const char* name, bool* open=nullptr, ImGuiTabItemFlags flags=0) : scoped_effect(ImGui::BeginTabItem(name, open, flags), ImGui::EndTabItem) {}
+    TabItem(const char* name, bool* open=nullptr, ImGuiTabItemFlags flags=0)
+        : ScopeWrapper(ImGui::BeginTabItem(name, open, flags)) {}
+    static constexpr void(*dtor)() = ImGui::EndTabItem;
 };
 
 }
